af11d6200ef53e5257fe3dbcb36386aeab42eef6
e1ka <e1kaczrw@gmail.com>
2018-07-26 23:53:45 +0200

    Add highscore

--- 2DSwingJumper/Assets/Scripts/GameController.cs
+++ 2DSwingJumper/Assets/Scripts/GameController.cs
@@ -5,22 +5,36 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-    public Text scoreText, gameOverText;
+    public Text scoreText, highScoreText, gameOver;
 
     private bool gameover, restart;
-    private int score;
+    private int score, highScore;
 
 	void Start ()
     {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        DisplayHighScore();
         gameover = false;
         restart = false;
         score = 0;
         UpdateScore();
     }
 
+    private void DisplayHighScore()
+    {
+        highScoreText.text = highScore.ToString();
+    }
+
     private void UpdateScore()
     {
         scoreText.text = score.ToString();
+        if(score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", score);
+            DisplayHighScore();
+        }
+            
     }
     public void AddScore(int newScoreValue)
     {
@@ -29,7 +43,7 @@ public class GameController : MonoBehaviour {
     }
     public void GameOver()
     {
-        gameOverText.text = "GAME OVER";
+        gameOver.text = "GAME OVER";
         gameover = true;
     }
 }
